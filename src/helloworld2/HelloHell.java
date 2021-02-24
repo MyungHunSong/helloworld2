591b04253338e9c6919fe8b170276bcd3964a1f3
MyungHunSong <change1677@naver.com>
2021-02-24 18:07:22 +0900

    저으장

--- src/helloworld2/HelloHell.java
+++ src/helloworld2/HelloHell.java
@@ -5,7 +5,8 @@ public class HelloHell {
 	public static void main(String[] args) {
 		
 		System.out.println("Wellcome To Hell!");
-
+		System.out.println("get out of my place");
+		System.out.println("gOTTT");
 	}
 
 }
